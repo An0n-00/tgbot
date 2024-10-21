7b078135ab3fd24483a349b2af145ba3aad689ed
Anon <142223573+An0n-00@users.noreply.github.com>
2024-10-21 08:56:37 +0200

    Update Bot.java

--- src/main/java/TelegramBot/Bot.java
+++ src/main/java/TelegramBot/Bot.java
@@ -3,13 +3,8 @@ package TelegramBot;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.api.objects.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
     @Override
